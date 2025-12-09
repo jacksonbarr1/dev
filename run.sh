d59d14efd6303f040aebccdaa6423d8d1cad019f
Jackson Barr <jacksonbarr2021@gmail.com>
2025-12-09 16:04:31 -0500

    Check for args

--- run.sh
+++ run.sh
@@ -5,3 +5,26 @@ if [ -z "$DEV_ENV" ]; then
     echo "Env var DEV_ENV needs to be present."
     exit 1
 fi
+
+grep = ""
+dry_run = ""
+
+while [[ $# -gt 0]]; do
+    echo "ARG: \"$1\""
+    if [[ "$1" == "--dry" ]]; then
+        dry_run="1"
+    else
+        grep="$1"
+    fi
+    shift
+done
+
+log() {
+    if [[ $dry_run == "1" ]]; then
+        echo "[DRY_RUN]: $1"
+    else
+        echo "$1"
+    fi
+}
+
+log "RUN: env: $env -- grep: $grep"
