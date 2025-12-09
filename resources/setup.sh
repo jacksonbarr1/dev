c5af0bd055c87c4cb9608e1964d6f5601402affd
Jackson Barr <jacksonbarr2021@gmail.com>
2025-12-09 15:32:15 -0500

    feat: Create initializer script

---
+++ resources/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+apt update && apt install -y sudo
+
+# # Check if the script is running as root (EUID = 0)
+# # This applies for when testing in a Docker container
+# if [ $EUID -eq 0 ]; then
+#     echo "Started as root, creating user"
+#
+#     sudo useradd -m jackson # -m creates the home directory
+#
+#     echo "[!] Setting user password:"
+#     passwd jackson
+#
+#     sudo usermod -aG sudo jackson
+#
+#     echo "Rerun script from jackson shell."
+#     su - jackson
+# fi
+
+if ! command -v git &>/dev/null; then
+    sudo apt -y install git
+fi
+
+if [ ! -d $HOME/personal ]; then
+    mkdir $HOME/personal
+fi
+
+git clone https://github.com/jacksonbarr1/dev $HOME/personal/dev
+
+pushd $HOME/personal/dev
+echo "Initialization completed, running scripts..."
