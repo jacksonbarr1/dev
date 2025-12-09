68b3b71b3dc156d52baf7b4153d06e61b5cdf18c
Jackson Barr <jacksonbarr2021@gmail.com>
2025-12-09 16:04:16 -0500

    Uncomment user creation for docker testing

--- resources/setup.sh
+++ resources/setup.sh
@@ -2,21 +2,21 @@
 
 apt update && apt install -y sudo
 
-# # Check if the script is running as root (EUID = 0)
-# # This applies for when testing in a Docker container
-# if [ $EUID -eq 0 ]; then
-#     echo "Started as root, creating user"
-#
-#     sudo useradd -m jackson # -m creates the home directory
-#
-#     echo "[!] Setting user password:"
-#     passwd jackson
-#
-#     sudo usermod -aG sudo jackson
-#
-#     echo "Rerun script from jackson shell."
-#     su - jackson
-# fi
+# Check if the script is running as root (EUID = 0)
+# This applies for when testing in a Docker container
+if [ $EUID -eq 0 ]; then
+    echo "Started as root, creating user"
+
+    sudo useradd -m jackson # -m creates the home directory
+
+    echo "[!] Setting user password:"
+    passwd jackson
+
+    sudo usermod -aG sudo jackson
+
+    echo "Rerun script from jackson shell."
+    su - jackson
+fi
 
 if ! command -v git &>/dev/null; then
     sudo apt -y install git
@@ -30,5 +30,6 @@ git clone https://github.com/jacksonbarr1/dev $HOME/personal/dev
 
 cd $HOME/personal/dev
 chmod +x run.sh
+export DEV_ENV=$(pwd)
 echo "Initialization completed, running scripts..."
 ./run.sh
