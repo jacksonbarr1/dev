b700a9eb671a21d93639b173f24b4711df2c56b6
Jackson Barr <jacksonbarr2021@gmail.com>
2025-12-09 17:19:20 -0500

    Feat: build/install neovim

--- runs/neovim.sh
+++ runs/neovim.sh
@@ -1,21 +1,45 @@
 #!/usr/bin/env bash
 
-echo "Removing existing neovim installation and configuration..."
-sudo rm -rf $HOME/personal/src/neovim
-sudo rm -rf $HOME/.config/nvim
-sudo rm -rf $HOME/.local/share/nvim
-sudo rm -rf $HOME/.local/state/nvim
-sudo rm -rf /usr/local/bin/nvim
-sudo rm -rf /usr/local/share/nvim
+NEOVIM_TAG="stable"
+INSTALL_DIR="/usr/local/bin/nvim"
+SOURCE_DIR="$HOME/src/neovim"
 
-echo "Cloning neovim (stable)..."
-git clone --depth 1 --branch stable https://github.com/neovim/neovim.git $HOME/personal/src/neovim
+if [ -f "$INSTALL_DIR" ]; then
+    echo "Neovim binary found at $INSTALL_DIR. Skipping compilation..."
+    # TODO: Version check
+else
+    echo "Neovim not found. Proceeding with installation."
 
-mkdir -p $HOME/neovim
-pushd $HOME/personal/src/neovim
-echo "Building neovim from source..."
+    echo "Installing required build dependencies..."
+    sudo apt install -y curl wget build-essential cmake gettext libtool libtool-bin
 
-rm -r build/ # clear CMake cache
-make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
-make install
-export PATH="$HOME/neovim/bin:$PATH"
+    echo "Cloning and checkout out tag $NEOVIM_TAG..."
+    mkdir -p "$HOME/src"
+
+    if [ ! -d "$SOURCE_DIR" ]; then
+        git clone https://github.com/neovim/neovim.git "$SOURCE_DIR"
+    fi
+
+    cd "$SOURCE_DIR"
+    git fetch --tags
+    git checkout "$NEOVIM_TAG"
+
+    echo "Compiling Neovim..."
+    rm -rf build
+
+    make CMAKE_BUILD_TYPE=RelWithDebInfo || {
+        echo "Compilation failed. Exiting."
+        exit1
+    }
+
+    echo "Installing to $INSTALL_DIR..."
+    sudo make install
+fi
+
+if [ -f "$INSTALL_DIR" ]; then
+    echo "Neovim successfully installed:"
+    $INSTALL_DIR --version | head -n 1
+else
+    echo "Neovim installation failed."
+    exit 1
+fi
